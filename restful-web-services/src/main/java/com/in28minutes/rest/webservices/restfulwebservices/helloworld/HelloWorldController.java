faa2010caa8a27a83143f54a9c3d5895e21449e4
Linjing Li <linjingli2012@gmail.com>
2024-04-29 00:26:54 -0700

    1. customize error handling

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+	
+	// /hello-world -> "Hello World"
+	
+//	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+}
