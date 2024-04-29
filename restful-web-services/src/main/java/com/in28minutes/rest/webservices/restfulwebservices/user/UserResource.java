212aee0b652e6ab46427d84f2c8ed226e6d1e72b
Linjing Li <linjingli2012@gmail.com>
2024-04-29 00:37:38 -0700

    2. add delete api

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,13 @@ public class UserResource {
 		if (user==null)
 			throw new UserNotFoundException("id:" + id);
 		return user;
+	}
+	
+	// delete specific user
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		service.deleteById(id);
+		
 	}
 	
 	// create a new User
